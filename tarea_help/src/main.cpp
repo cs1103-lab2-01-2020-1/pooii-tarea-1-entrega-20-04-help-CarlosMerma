ff91e20f9fc324d280e0d522b4c77facfc8e2a5f
CarlosMerma08 <40005066+CarlosMerma@users.noreply.github.com>
2020-04-20 11:10:15 -0500

    sd

--- tarea_help/src/main.cpp
+++ tarea_help/src/main.cpp
@@ -1,34 +1,22 @@
+//
+// Author: marvin on 4/19/20.
+//
 
 #include "lib.h"
-#include "template/stack.h"
-#include "template/stack.cpp"
-
-#include "lib.h"
-#include "template/stack.h"
-
 
 int main() {
 
-    stack<int> intstack;
-    stack<string> stringstack;
+    Stack<int> intstack;
+    Stack<string> stringstack;
 
     intstack.push(100);
-
-    cout<<intstack.top()<<endl;
-    intstack.push(500);
-    cout<<intstack.top()<<endl;
-    intstack.pop();
-    cout<<intstack.top()<<endl;
-    
-
+    cout<<<intstack.top()<<endl;
 
     stringstack.push("utec");
     cout<<stringstack.top()<<endl;
-    stringstack.push("La mejor universidad");
-    cout<<stringstack.top()<<endl;
     stringstack.pop();
-    cout<<stringstack.top()<<endl;
 
+    //agregar más ejemplos de todas las funciones de Stack
 
     return 0;
 }
